6187de108ee39763e8afe988981d4f6210008f67
lhjrobo <63150314+lhjrobo@users.noreply.github.com>
2020-05-04 01:11:50 +0900

    .

---
+++ 20200503_build/precache-manifest.13c5dd4ff200d41eccc1f0310803c532.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f22aae6cae8a4871c698bd0682480c46",
+    "url": "./index.html"
+  },
+  {
+    "revision": "cc50a4036923160d4cc5",
+    "url": "./static/css/main.c87e1df9.chunk.css"
+  },
+  {
+    "revision": "7fbf6c41d59e0d374b88",
+    "url": "./static/js/2.2d8c1bea.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.2d8c1bea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc50a4036923160d4cc5",
+    "url": "./static/js/main.d54696e1.chunk.js"
+  },
+  {
+    "revision": "5e294297bb7728118280",
+    "url": "./static/js/runtime-main.f10eadef.js"
+  },
+  {
+    "revision": "8066dadab1135a573edb0a57258e51f8",
+    "url": "./static/media/Yam_greetings.8066dada.PNG"
+  },
+  {
+    "revision": "40b7a4abbc3f8b5e2ba7163f49e088fc",
+    "url": "./static/media/logo2.40b7a4ab.png"
+  }
+]);
